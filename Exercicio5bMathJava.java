72807d9e0deaf28784a20464c5bc346b562b53d1
AndriyBrine <163847323+AndriyBrine@users.noreply.github.com>
2025-03-25 16:18:05 -0300

    Update Exercicio5bMathJava.java

--- Exercicio5bMathJava.java
+++ Exercicio5bMathJava.java
@@ -3,25 +3,26 @@ import java.lang.Math;
 public class Exercicio5{ 
 
 	public static void main (String[] args){
-        Scanner teclado = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 	    int l1, l2, l3;
-        l1 = teclado.nextInt();
-        l2 = teclado.nextInt();
-        l3 = teclado.nextInt();
+	System.out.print("Valor de Lado 1: ");
+        l1 = scanner.nextInt();
+        System.out.print("Valor de Lado 2: ");
+        l2 = scanner.nextInt();
+	System.out.print("Valor de Lado 3: ");
+        l3 = scanner.nextInt();
+        
         if (!(l1+l2 <= l3 || l2+l3<=l1 || l3+l1<=l2)){
             System.out.print("É triângulo! ");
-            if(Math.pow(l1,2) == Math.pow(l2,2) + Math.pow(l3,2)){
-                System.out.print("É retângulo! ");
-            } else if (Math.pow(l2,2) == Math.pow(l3,2) + Math.pow(l1,2)){
-                System.out.print("É retângulo! ");
-            } else if (Math.pow(l3,2) == Math.pow(l1,2) + Math.pow(l2,2)){
+            
+            if(Math.pow(l1,2) == Math.pow(l2,2) + Math.pow(l3,2) || Math.pow(l2,2) == Math.pow(l3,2) + Math.pow(l1,2 ) || Math.pow(l3,2) == Math.pow(l1,2) + Math.pow(l2,2)){
                 System.out.print("É retângulo! ");
             } else{
                 System.out.print("Não é retângulo! ");
             }
-        	} else {
+        } else {
             System.out.print("Não é triângulo! ");
         }
-		scanner.close();
+    		scanner.close();
 	}
 }
