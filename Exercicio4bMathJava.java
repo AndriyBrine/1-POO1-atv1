88ff78c2370b773dccdf1f8165d848d18b219786
AndriyBrine <163847323+AndriyBrine@users.noreply.github.com>
2025-03-25 16:04:42 -0300

    Update Exercicio4bMathJava.java

--- Exercicio4bMathJava.java
+++ Exercicio4bMathJava.java
@@ -2,15 +2,15 @@ import java.lang.Math;
 public class Exercicio4{ 
 
 	public static void main (String[] args){
-        int q = 1;
-        int V = 0;
-        int S;
+        float q = 1;
+        float V = 0;
+        float S;
         
         for(int i = 38; i >= 2; i--){
             S = (i-1) * i/q;
             V = V + S;
             q = q+1;
         }
-        System.out.print(V);
+        System.out.printf("%.2f",Math.sqrt(V));
 	}
 }
