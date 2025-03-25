d92948599ae831e220754a93a873904b5f45fcb0
AndriyBrine <163847323+AndriyBrine@users.noreply.github.com>
2025-03-25 00:38:30 -0300

    Create Exercicio1bScanner.java

---
+++ Exercicio1bScanner.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.lang.Math;
+public class Exercicio4{ 
+
+	public static void main (String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Temperatura Celsius = ");
+        int C = scanner.nextInt();
+        double F, K; // Pois são decimais
+        F = (C * 1.8) + 32;
+        K = C + 273;
+        System.out.printf("Fahrenheit = %.2f ", F);
+        System.out.println("");
+        System.out.printf("Kelvin = %.2f", K);
+        
+	}
+}
