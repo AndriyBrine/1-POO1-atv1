944a8704895de6edb6df77cda75b32a7238b6988
AndriyBrine <163847323+AndriyBrine@users.noreply.github.com>
2025-03-24 22:25:44 -0300

    Create Exercicio3.java

---
+++ Exercicio3.java
@@ -0,0 +1,20 @@
+import java.lang.Math;
+import java.util.Random;
+public class Exercicio3{ 
+
+	public static void main (String[] args){
+	    Random random = new Random();
+        for (int i = 1; i <=10; i++){
+            int x1 = random.nextInt();
+            int y1 = random.nextInt();
+            int x2 = random.nextInt();
+            int y2 = random.nextInt();
+            double dstnc = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+            System.out.printf("%.2f, ", x1);
+            System.out.printf("%.2f e ",y1);
+            System.out.printf("%.2f,",x2);
+            System.out.printf("%.2f, distância: %.2f", y2, dstnc);
+            System.out.println("");
+        }
+    }
+}
